7b7c53920aa6fdb1710cfb0c707471cf1326f8c7
JUBAYER AHMMED <jubayerahmmed105@gmail.com>
2026-01-03 19:36:08 +0600

    frontend design step

---
+++ version0/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,include
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
+from django.conf import settings
+from core.views import home
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('courses/', include("courses.urls")),
+    path('', home, name = 'home'),
+]+ debug_toolbar_urls()
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
